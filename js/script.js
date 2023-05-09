4e1247852ad5116cfd8e0de487b0eb10a7a5ef5c
Andrey Home <example@gmail.com>
2023-05-10 02:38:11 +0300

    ready maket

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const btnSlider = document.querySelector('.btn__arrow');
+    let count = 1;
+    
 
+    btnSlider.addEventListener('click', () => {
+        const numberPage = document.querySelectorAll('.slider__number');
+        const sliderLine = document.querySelector('.slider__line');
+        const sliderScroll = sliderLine.querySelector('span');
+        count++;  
+        if(count <= 6){
+            let widthScroll = 40;
+            numberPage[0].innerHTML = `0${count}`;
+            sliderScroll.style.top = `${widthScroll * count}px`;
+        }
+    });
     
 });
